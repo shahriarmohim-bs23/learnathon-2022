f426762b2d0d49d1fbef524a45a79622e519e193
nasif <nasifshahriar4@gmail.com>
2022-06-30 22:45:20 +0600

    1st commit

---
+++ backend_task3/Minimamage.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_task3
+{
+    public class Minimamage : ValidationAttribute
+    {
+        int _minage;
+        public Minimamage(int minage)
+        {
+            _minage = minage;
+        }
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.AddYears(_minage) < DateTime.Now;
+            }
+            return false;
+        }
+    }
+}
---
+++ backend_task3/Program.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using backend_task3;
+using backend_task3.Models;
+using backend_task3.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection(("MongoDbSettings")));
+builder.Services.AddSingleton<Userservices>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddCors(options =>
+{
+   options.AddPolicy("AllowSpecificOrigins",
+                         policy =>
+                         {
+                             policy.WithOrigins("*")
+                                             .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                         });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseRouting();
+app.UseCors("AllowSpecificOrigins");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
