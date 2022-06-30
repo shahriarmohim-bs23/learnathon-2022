38393fbb2f6f900ff6a289f183e5afa01f5cfc7f
nasif <nasifshahriar4@gmail.com>
2022-06-30 22:42:42 +0600

    2nd commit

--- backend_task3/Models/loginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace backend_task3.Models
-{
-    public class loginDto
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
